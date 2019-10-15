d5fabceb5141fca4bb8ff994db6e84a9eb314d63
Emiliano <inbox@emilianomaccaferri.com>
2019-10-15 11:09:02 +0200

    removed a useless Promise construct in autopilot.js, less memory consumption

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const Crawler = require("./lib/Crawler");
 var crawler = new Crawler({
   threads: 10, // 10 threads to spawn
-  timeout: 60 // 60 seconds of delay between each request
+  timeout: 20 // 20 seconds of delay between each request
 });
-
-console.log("we be crawlin' down here");
 
 crawler.start();
 
 crawler.on('error', data => {
 
-  console.log(`${data.id} has reported an error: `);
-  console.log(data.error);
+  console.log(`${data.thread_id} has reported an error: `);
+  console.error(data.error);
 
 })
 
 crawler.on('message', msg => {
 
-  if(msg.type === 'work_done') console.log(`Oh well, thread #${msg.thread_id} has cached ${msg.results.length} more items!`);
+  if(msg.type === 'work_done') console.log(`thread #${msg.thread_id} has cached ${msg.results.length} more items`);
   if(msg.type === 'reset') console.log(`Resetting everything, we scanned all the market!`);
 
 })
